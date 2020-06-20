f8f26258b4e0f6454218a1cc7fb0e8c366a9bea2
Dk for Dark <dkfordark@gmail.com>
2020-06-20 12:22:50 +0700

    Done excercise 1

--- excercise.py
+++ excercise.py
@@ -1,7 +1,16 @@
 # https://quantrimang.com/hon-100-bai-tap-python-co-loi-giai-code-mau-142456
 # bai 1: 
-
+'''
 j = [];
 
 for i in range(2000, 3200):
-    if ((i%7)===0)
+    if (i%7==0) and (i%5!=0):
+        j.append(str(i));
+
+print (','.join(j));
+'''
+
+# bai 2:
+
+
+
